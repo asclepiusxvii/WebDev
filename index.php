22e05b4f24f2b5a2934a0b48884bbb5a9cc41b9d
asclepiusxvii <asclepiusxvii@gmail.com>
2026-09-10 15:52:51 +0800

    B4 Presentation

--- index.php
+++ index.php
@@ -420,6 +420,7 @@ $recentRequests = $conn->query("
 
 <footer class="bg-[#121312] px-6 pb-10 text-center text-sm text-white/40">
   © 2026 RamTech. All rights reserved. Solutions Made Simple.
+  Contact: ramtech.solutions2026@gmail.com / +63 947 786 2798
 </footer>
 
 <script src="/ramtech/assets/js/app.js"></script>
